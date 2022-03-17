f34e62a286adbdbf75d3dc39ae7b4999f57402b8
Agustin Vanetta <agvanetta@gmail.com>
2022-03-17 16:02:20 -0300

    jdk

--- Clase 17 - Evaluacion I Proyecto/1. Vanetta_Agustin/src/Main.java
+++ Clase 17 - Evaluacion I Proyecto/1. Vanetta_Agustin/src/Main.java
@@ -1,16 +1,4 @@
-import daos.OdontologoDAOH2;
-import entidades.Odontologo;
-import servicios.OdontologoService;
-
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-
-        OdontologoService odontologoService = new OdontologoService();
-        odontologoService.setOdontologoInterfaceDAO(new OdontologoDAOH2());
-
-        List<Odontologo> od = odontologoService.buscarTodos();
-        System.out.println(od);
     }
 }
