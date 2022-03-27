41bbb8a4be14fc6f54be2f5d49491cd7f8e837dc
Agustin Vanetta <agvanetta@gmail.com>
2022-03-27 15:43:06 -0300

    ApiRest

--- 4. Spring - Clase 21 a/Clase 25 - API REST/1. calendar/src/main/java/com/example/calendar/controller/Controller.java
+++ 4. Spring - Clase 21 a/Clase 25 - API REST/1. calendar/src/main/java/com/example/calendar/controller/Controller.java
@@ -1,4 +1,47 @@
 package com.example.calendar.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+import java.util.Locale;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+import static java.time.temporal.ChronoUnit.YEARS;
+
+@RestController
+@org.springframework.stereotype.Controller
 public class Controller {
+
+    @GetMapping("calendar/{fecha}") // "2020-01-15"
+    public String devolverFecha(@PathVariable("fecha") String requestDate) {
+        //requestDate = "2020-01-15";
+        LocalDate myDate = LocalDate.parse(requestDate);
+        LocalDate currentDate = LocalDate.now();
+
+        long numberOFDays = YEARS.between(myDate, currentDate);
+        return "Tu edad es " + numberOFDays;
+    }
+
+    @GetMapping("/{numero}")
+    public String suma(@PathVariable("numero") int numeroAlAzar) {
+        int suma = 10+numeroAlAzar;
+        return "el resultado es =" +suma;
+    }
+
+    @GetMapping("/hola")
+    public String sum2(int random) {
+        int suma = 10+random;
+        return "el resultado es =" +suma;
+    }
+
+    @GetMapping("/hola2")
+    public String saludo(){
+        return "hola";
+    }
 }
