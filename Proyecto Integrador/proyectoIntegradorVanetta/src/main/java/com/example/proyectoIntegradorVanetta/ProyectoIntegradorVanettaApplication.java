046f1f30093848b7d171ea2fdeb27f6f4cf94b72
Agustin Vanetta <agvanetta@gmail.com>
2022-03-29 19:11:18 -0300

    Integrador

---
+++ Proyecto Integrador/proyectoIntegradorVanetta/src/main/java/com/example/proyectoIntegradorVanetta/ProyectoIntegradorVanettaApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoIntegradorVanetta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradorVanettaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegradorVanettaApplication.class, args);
+	}
+
+}
---
+++ Proyecto Integrador/proyectoIntegradorVanetta/src/test/java/com/example/proyectoIntegradorVanetta/ProyectoIntegradorVanettaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoIntegradorVanetta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradorVanettaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
